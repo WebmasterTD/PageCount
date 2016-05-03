b09d4b1d11dd0f4271401e56df840b3bb153e18f
webmastertd <webmastertd000@gmail.com>
2016-05-03 23:38:29 +0200

    Better commented version.

--- counter.py
+++ counter.py
@@ -4,6 +4,65 @@ import signal
 from libs import trap
 from libs import counter
 
+def init_codes():
+    code = list()
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.1.1"))                  #black_a4_copy
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.1.1"))                  #black_a3_copy
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.1.2"))                  #black_a4_print
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.1.2"))                  #black_a3_print
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.2.1"))                  #color_a4_copy
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.2.1"))                  #color_a4_print
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.2.2"))                  #color_a3_copy
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.2.2"))                  #color_a3_print
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.3.1.5.1"))                    #scan_a4
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.3.1.6.1"))                    #scan_a3
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.3.0"))                      #duplex
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.10.0"))                     #pages
+    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.8.0"))                      #originals
+    return code
+
+def get_all_delta():
+    delta = list()
+    for code in codes:
+        delta.append(code.delta())
+    return delta
+
+def deltas_2_values(delta):
+    values = list()
+    values.append((delta[0] + delta[2]) - (2 * (delta[1] + delta[3])))  #BW A4
+    values.append(delta[1] + delta[3])                                  #BW A3
+    values.append((delta[4] + delta[6]) - (2 * (delta[5] + delta[7])))  #CL A4
+    values.append(delta[5] + delta[7])                                  #CL A3
+    values.append(delta[8] + delta[9])                                  #SCAN
+    values.append(0)                                                    #D BW A4
+    values.append(0)                                                    #D BW A3
+
+    if delta[10] > 0:
+        if (2 * delta[10]) == values[0]:
+            values[0] = values[0] - (2 * delta[10])
+            values[5] = delta[10]
+        elif (2 * delta[10]) == values[0]:
+            values[1] = values[1] - (2 * delta[10])
+            values[6] = delta[10]
+
+    ids = delta[12] - delta[11]          #originals - pages
+    if ids > 0 and values[0] == delta[0]:
+        values[0] = values[0] - (ids)
+        values[5] = ids
+    return values
+
+def all_sum(values):
+    global allsums
+    for i in xrange(len(values)):
+        allsums[i] += values[i]
+    return allsums
+
+def values_2_str(values):
+    s = ""
+    for i in xrange(len(values)):
+        s += str(values[i]) + "\n"
+    return s
+
 def receive_signal(signum, stack):
     print "got signal"
     deltas = get_all_delta()
@@ -53,68 +112,9 @@ def reset_button(event):
     #job_label.configure(text = updated_values)
     total_label.configure(text = updated_totals)
 
-def init_codes():
-    code = list()
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.1.1"))                  #black_a4_copy
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.1.1"))                  #black_a3_copy
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.1.2"))                  #black_a4_print
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.1.2"))                  #black_a3_print
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.2.1"))                  #color_a4_copy
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.2.1"))                  #color_a4_print
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.5.2.2"))                  #color_a3_copy
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.2.1.7.2.2"))                  #color_a3_print
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.3.1.5.1"))                    #scan_a4
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.3.1.6.1"))                    #scan_a3
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.3.0"))                      #duplex
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.10.0"))                     #pages
-    code.append(counter(".1.3.6.1.4.1.18334.1.1.1.5.7.2.1.8.0"))                      #originals
-    return code
-
-def get_all_delta():
-    delta = list()
-    for code in codes:
-        delta.append(code.delta())
-    return delta
-
-def deltas_2_values(delta):
-    values = list()
-    values.append((delta[0] + delta[2]) - (2 * (delta[1] + delta[3])))  #BW A4
-    values.append(delta[1] + delta[3])                                  #BW A3
-    values.append((delta[4] + delta[6]) - (2 * (delta[5] + delta[7])))  #CL A4
-    values.append(delta[5] + delta[7])                                  #CL A3
-    values.append(delta[8] + delta[9])                                  #SCAN
-    values.append(0)                                                    #D BW A4
-    values.append(0)                                                    #D BW A3
-
-    if delta[10] > 0:
-        if (2 * delta[10]) == values[0]:
-            values[0] = values[0] - (2 * delta[10])
-            values[5] = delta[10]
-        elif (2 * delta[10]) == values[0]:
-            values[1] = values[1] - (2 * delta[10])
-            values[6] = delta[10]
-
-    ids = delta[12] - delta[11]          #originals - pages
-    if ids > 0 and values[0] == delta[0]:
-        values[0] = values[0] - (ids)
-        values[5] = ids
-    return values
-
 def reset_job():
     for code in codes:
         code.job_reset()
-
-def all_sum(values):
-    global allsums
-    for i in xrange(len(values)):
-        allsums[i] += values[i]
-    return allsums
-
-def values_2_str(values):
-    s = ""
-    for i in xrange(len(values)):
-        s += str(values[i]) + "\n"
-    return s
 
 def refresher():
     root.update_idletasks()
@@ -141,13 +141,11 @@ root.overrideredirect(True)
 root.config(cursor='none')
 
 text_label = Label(root, text=text, font=("Arial", 60), padx=80, pady=130, justify=RIGHT)
-#job_label = Label(root, text="0", font=("Arial", 50), padx=0, pady=80)
 total_label = Label(root, text="0", font=("Arial", 60), padx=10, pady=130)
 
 root.bind("<Button-1>", reset_button)
-root.bind("<Button-3>", exit_gracefully)
+root.bind("<Shift_L>", exit_gracefully)
 text_label.grid(row=1,column=0,sticky=N)
-#job_label.grid(row=1,column=1,sticky=N)
 total_label.grid(row=1,column=2,sticky=N)
 
 refresher()
--- libs.py
+++ libs.py
@@ -3,20 +3,18 @@ from pysnmp.carrier.asyncore.dispatch import AsyncoreDispatcher
 from pysnmp.carrier.asyncore.dgram import udp
 import os
 import signal
-
+                            #PRINTER IP
 session = Session(hostname='192.168.1.70', community='public', version=2)
 
 class trap:
-
+    #Callback function fro SNMP Trap
     def cbFun(self, transportDispatcher, transportDomain, transportAddress, wholeMsg):
-        #print os.getpid()
-        #print "job done"
         os.kill(os.getpid(), signal.SIGUSR1)
-
+    #SNMP Trap init setup
     def __init__(self):
         transportDispatcher = AsyncoreDispatcher()
         transportDispatcher.registerRecvCbFun(self.cbFun)
-        transportDispatcher.registerTransport(udp.domainName, udp.UdpSocketTransport().openServerMode(('192.168.1.111', 162))) #PC IP ADRESS
+        transportDispatcher.registerTransport(udp.domainName, udp.UdpSocketTransport().openServerMode(('192.168.1.111', 162))) #RASPBERRY   /   PC IP ADRESS
         transportDispatcher.jobStarted(1)
         try:
             transportDispatcher.runDispatcher()
@@ -28,16 +26,14 @@ class counter():
     def __init__(self, snmp):
         self.snmp = snmp
         self.previous = self.get_counter()
-
+    #Read snmp value
     def get_counter(self):
         count = int(session.get(self.snmp).value)
         return count
-
+    #Difference in values from last reset
     def delta(self):
         delt = self.get_counter() - self.previous
         return delt
-
+    #Reset counter
     def job_reset(self):
         self.previous = self.get_counter()
-
-
